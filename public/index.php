8ecfaa02168e1e7700b37b181a1900b9180c793b
李阳 <liyang@liyangdeMacBook-Pro.local>
2019-01-19 15:33:21 +0800

    将.idea等不必要文件从版本库中删除

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+phpinfo();die;
+
 define('LARAVEL_START', microtime(true));
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +13,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('student.welcome');
+});
+
+Route::get('/home', function () {
+   return 'home';
 });
